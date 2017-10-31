7e41b56b1b97d0f6849f483f8eaeb872971ea587
Andrew Santosa <santosa_1999@yahoo.com>
2017-10-31 21:58:08 +0800

    Removed klee API from simple_loop.c

--- simple_loop.c
+++ simple_loop.c
@@ -1,24 +1,17 @@
 /*
- * Simple loop example for numerical precision analysis.
+ * Simple loop example.
  *
  * Copyright 2017 National University of Singapore
  *
  * See LICENSE.md for license information.
  */
 
-#include <klee/klee.h>
-
 int main(int argc, char **argv) {
   int c = 0;
-
-  klee_make_symbolic(&c, sizeof(c), "c");
-  klee_track_error(&c, "c_error");
 
   for (int i = 0; i < 3; ++i) {
     c++;
   }
-
-  klee_bound_error(c, 1.3);
 
   return 0;
 }
